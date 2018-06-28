9a40557a693db15146ba199424b7997e4f8251b2
zhanweichao <921391554@qq.com>
2018-06-28 14:16:00 +0800

    线平移

--- src/js/demo3.js
+++ src/js/demo3.js
@@ -15,7 +15,7 @@ const {
 const scene = new Three.Scene()
 // camera
 const camera = new Three.PerspectiveCamera(45, width / height, 1, 10000)
-camera.position.set(0, 0, 3)
+camera.position.set(0, 0, 10000)
 
 // light
 const light = new Three.DirectionalLight(0xffffff, 1.0, 0)
@@ -31,27 +31,44 @@ const matirial = new Three.LineBasicMaterial({
   color: 0xffffff
 })
 const color1 = new Three.Color(0x000000), color2 = new Three.Color(0xff0000)
-const point1 = new Three.Vector3(0, 0, 0), point2 = new Three.Vector3(0, 1, 0), point3 = new Three.Vector3(1, 0, 0)
+const point1 = new Three.Vector3(-3000, -2000, 0), point2 = new Three.Vector3(0, 2000, 0), point3 = new Three.Vector3(3000, -2000, 0)
 
 geometry1.vertices.push(point1, point2)
 geometry1.colors.push(color1, color2)
 
-geometry1.vertices.push(point1, point3)
-geometry1.colors.push(color1, color2)
+geometry2.vertices.push(point1, point3)
+geometry2.colors.push(color1, color2)
 
-geometry1.vertices.push(point2, point3)
-geometry1.colors.push(color1, color2)
+geometry3.vertices.push(point2, point3)
+geometry3.colors.push(color1, color2)
 
-const line1 = new Three.Line(geometry1, matirial, Three.LinePieces)
-const line2 = new Three.Line(geometry2, matirial, Three.LinePieces)
-const line3 = new Three.Line(geometry3, matirial, Three.LinePieces)
+for (let i = 0; i < 10; i++) {
+  const line1 = new Three.Line(geometry1, matirial, Three.LinePieces)
+  const line2 = new Three.Line(geometry2, matirial, Three.LinePieces)
+  const line3 = new Three.Line(geometry3, matirial, Three.LinePieces)
+
+  line1.position.y = 200 * i 
+  line2.position.y = 200 * i 
+  line3.position.y = 200 * i 
+
+  scene.add(line1)
+  scene.add(line2)
+  scene.add(line3)
+}
+
 
 // build
-scene.add(line1)
-scene.add(line2)
-scene.add(line3)
 scene.add(light)
 scene.add(camera)
 
 renderer.clear()
 renderer.render(scene, camera)
+
+// function runAnimation() {
+//   camera.position.x = camera.position.x + 1;
+//   camera.position.y = camera.position.y + 1;
+  
+//   requestAnimationFrame(runAnimation);
+// }
+
+// runAnimation()
