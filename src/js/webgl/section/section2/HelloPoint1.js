14480f9eaef37557407ba5161cecf05f8a079064
weichaozhan <921391554@qq.com>
2019-07-04 18:07:19 +0800

    a

--- src/js/webgl/section/section2/HelloPoint1.js
+++ src/js/webgl/section/section2/HelloPoint1.js
@@ -5,8 +5,22 @@ import {
   getWebGLContext,
 } from '../../lib/cuon-utils.js';
 
+const VSHEADER_SOURCE = `
+  void main() {
+    gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+    gl_PointSize = 20.0;
+  }
+`;
+const FSHEADER_SOURCE = `
+  void main() {
+    gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);
+  }
+`;
+
 const canvas = document.querySelector('#HelloPoint1');
 const gl = getWebGLContext(canvas);
+
+initShaders(gl, VSHEADER_SOURCE, FSHEADER_SOURCE);
 
 gl.clearColor(0.0, 0.0, 0.0, 0.8);
 gl.clear(gl.COLOR_BUFFER_BIT);
